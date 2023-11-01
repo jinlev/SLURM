3e6c0a1338550dbd14a02742e6084d7e770599d9
jinleon <104912403+jinleon@users.noreply.github.com>
2023-11-01 11:12:03 -0600

    Create conda.py

---
+++ basic/conda.py
@@ -0,0 +1,9 @@
+import numpy as np
+import tensor as tf  ### error  wrong typing                                    
+import sklearn
+import cv2
+
+quote = (12*("*")+ ("\n")  + "All it's ok \n"+ 12*("*")+("\n"))
+
+with open('conda.out', 'w', encoding='utf-8') as f:
+    f.write(quote)
